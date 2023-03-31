e3af74060b418c30914cb27dc8ca9c0fa6e5b83f
Marie Goa <mgoa90334@gmail.com>
2023-03-31 09:13:09 +0000

    new

--- javascript/connexion.js
+++ javascript/connexion.js
@@ -26,7 +26,7 @@
                 }
                 localStorage.setItem('sessionUser', JSON.stringify(session));
 
-                window.location.href = "../html/superAdmin.html"
+                window.location.href = "../html/superadmin.html"
             }
             else{
                 alert("le mot de passe incorrect")
@@ -41,34 +41,4 @@
 
 // -------------connexion administrateur------------------
 
-// boutonconnexion.addEventListener('click', (e)=>{
-//     e.preventDefault()
-
-//     let nomutilisateur = document.getElementById('email').value
-//     let motdepasse = document.getElementById('mdp').value
-
-//     let recupAdmin = localStorage.getItem("Administrateur");
-//     recupAdmin = JSON.parse(recupAdmin);
-//     console.log(recupAdmin);
-
-//     if (recupAdmin != null) {
-//         if (nomutilisateur === recupAdmin.user) {
-//             if (motdepasse === recupAdmin.pwd) {
-//                 let session = {
-//                     user : administrateur,
-//                     status : "Administrateur",
-//                     pwd : motdepasse
-//                 }
-//                 localStorage.setItem('sessionUser', JSON.stringify(session));
-//                 window.location.href = "../html/dashboardadmin.html"
-                
-//             }
-//             else{
-//                 alert("le mot de passe incorrect")
-//             }
-//         }
-//         else{
-//             alert("nom d'utilisatuer incorrect")
-//            }
-//     }
-// })
+// 
